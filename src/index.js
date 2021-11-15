2eec8dbb48d44262ed7f28ab4ee12be501b6e2cc
Ankit <ak656634@gmail.com>
2021-11-15 20:12:16 +0530

    Styled Table view of blood banks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const blood_banks = require('./utils/blood_bank')
-const request = require('request')
 require('./db/mongoose')
 
 const app = express()
@@ -24,22 +22,7 @@ app.use(express.static(publicPath))
 app.get('/Blood-Bud',(req,res)=>{
     res.render('home')
 })
-app.get('/blood_banks',(req,res)=>{
-    const url = "https://api.data.gov.in/resource/fced6df9-a360-4e08-8ca0-f283fc74ce15?api-key=579b464db66ec23bdd00000129d0b4eb7b284a5e69258ef4eb5b37a5&format=json&offset=0&limit=100&filters["+req.query.key+"]="+req.query.value
 
-    if(!req.query.value){
-        res.send("Please enter any location")
-    }
-    else{
-        request({url,json:true},(error,{body})=>{
-            if(error){
-                res.send("Error")
-            }else{
-                res.send(body.records)
-            }
-        })
-    }
-})
 app.listen(port,()=>{
     console.log("server is running!")
 })
